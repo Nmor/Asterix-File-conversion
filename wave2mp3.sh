b3af35b2b84af84c781dbdcf6baf439ca05aaa48
Nmor <nmor.moses@gmail.com>
2017-01-06 10:36:39 +0100

    Update wave2mp3.sh

--- wave2mp3.sh
+++ wave2mp3.sh
@@ -10,7 +10,7 @@ for file in *.wav; do
 mp3=$(basename "$file" .wav).mp3;
 nice lame -b 16 -m m -q 9-resample "$file" "$mp3";
 #touch --reference "$file" "$mp3";
-chown asterisk.asterisk "$mp3";
+chown root.root "$mp3";
 chmod 444 "$mp3";
 mv "$mp3" /var/www/html/monitor/mp3/2017/01/;
 rm -f "$file";
